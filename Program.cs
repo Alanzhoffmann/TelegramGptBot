53bf0a329fc1f8e3e2756c8d91770d8106cc4a3c
Alan Hoffmann <alanzhoffmann@gmail.com>
2023-03-29 23:22:41 +0100

    Setting up Telegram.Bot lib

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
 using TelegramGptBot;
 
 var config = new ConfigurationBuilder()
@@ -7,3 +9,28 @@ var config = new ConfigurationBuilder()
 
 var keys = config?.GetSection("Keys").Get<Keys>()
     ?? throw new Exception("Missing or invalid config file");
+
+var botClient = new TelegramBotClient(keys.TelegramBot);
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine(me.Username);
+
+using CancellationTokenSource cancellationTokenSource = new();
+
+botClient.StartReceiving((client, update, token) =>
+{
+    if (update.Message?.Type == MessageType.Text)
+    {
+        var message = update.Message;
+
+        var sentMessage = client.SendTextMessageAsync(message.Chat.Id, "Hello!", cancellationToken: token);
+    }
+},
+(client, exception, token) =>
+{
+
+}, cancellationToken: cancellationTokenSource.Token);
+
+Console.ReadLine();
+
+cancellationTokenSource.Cancel();
