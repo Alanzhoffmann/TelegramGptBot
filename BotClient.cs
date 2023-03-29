b02bf0d490b323b2a93e269cc505f12c428d1d62
Alan Hoffmann <alanzhoffmann@gmail.com>
2023-03-29 23:40:26 +0100

    Moving bot client methods to new class

---
+++ BotClient.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+
+namespace TelegramGptBot
+{
+    internal class BotClient
+    {
+        public TelegramBotClient _client;
+
+        public BotClient(IConfiguration configuration)
+        {
+            var keys = configuration?.GetSection("Keys").Get<Keys>()
+                ?? throw new Exception("Missing or invalid config file");
+
+            _client = new TelegramBotClient(keys.TelegramBot);
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken = default)
+        {
+            var me = await _client.GetMeAsync(cancellationToken: cancellationToken);
+            Console.WriteLine(me.Username);
+
+            _client.StartReceiving<UpdateHandler>(cancellationToken: cancellationToken);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,15 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Telegram.Bot;
 using TelegramGptBot;
 
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
 
-var keys = config?.GetSection("Keys").Get<Keys>()
-    ?? throw new Exception("Missing or invalid config file");
-
-var botClient = new TelegramBotClient(keys.TelegramBot);
-var me = await botClient.GetMeAsync();
-
-Console.WriteLine(me.Username);
-
 using CancellationTokenSource cancellationTokenSource = new();
 
-botClient.StartReceiving<UpdateHandler>(cancellationToken: cancellationTokenSource.Token);
+var client = new BotClient(config);
+
+await client.StartAsync();
 
 Console.ReadLine();
 
