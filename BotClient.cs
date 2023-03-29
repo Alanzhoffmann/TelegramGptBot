5b9041a9cda02792c0b61a1d99e86c0a1cd291d0
Alan Hoffmann <alanzhoffmann@gmail.com>
2023-03-29 23:47:31 +0100

    Adding logger

--- BotClient.cs
+++ BotClient.cs
@@ -1,24 +1,27 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 
 namespace TelegramGptBot
 {
     internal class BotClient
     {
+        private readonly ILogger<BotClient> _logger;
         public TelegramBotClient _client;
 
-        public BotClient(IConfiguration configuration)
+        public BotClient(IConfiguration configuration, ILogger<BotClient> logger)
         {
             var keys = configuration?.GetSection("Keys").Get<Keys>()
                 ?? throw new Exception("Missing or invalid config file");
 
             _client = new TelegramBotClient(keys.TelegramBot);
+            _logger = logger;
         }
 
         public async Task StartAsync(CancellationToken cancellationToken = default)
         {
             var me = await _client.GetMeAsync(cancellationToken: cancellationToken);
-            Console.WriteLine(me.Username);
+            _logger.LogInformation(me.Username);
 
             _client.StartReceiving<UpdateHandler>(cancellationToken: cancellationToken);
         }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using TelegramGptBot;
 
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
 
+var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+
+var logger = loggerFactory.CreateLogger<BotClient>();
+var client = new BotClient(config, logger);
+
 using CancellationTokenSource cancellationTokenSource = new();
 
-var client = new BotClient(config);
-
-await client.StartAsync();
+await client.StartAsync(cancellationTokenSource.Token);
 
 Console.ReadLine();
 
